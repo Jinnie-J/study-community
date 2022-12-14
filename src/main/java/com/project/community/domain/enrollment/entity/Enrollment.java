71331c37e59b141fb4fb99c802adbdd84f269ebe
Jinnie-J <jjesweet@naver.com>
2022-12-15 00:32:13 +0900

    Enrollment 엔티티 생성

---
+++ src/main/java/com/project/community/domain/enrollment/entity/Enrollment.java
@@ -0,0 +1,46 @@
+package com.project.community.domain.enrollment.entity;
+
+import com.project.community.domain.user.entity.User;
+import com.project.community.domain.user.entity.UserGroup;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+public class Enrollment {
+
+    @Id @GeneratedValue
+    @Column(name = "enrollment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_grou_id")
+    private UserGroup userGroup;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime enrolledAt;
+
+    private boolean accepted;
+
+    private boolean attended;
+
+    public void setUserGroup(UserGroup userGroup) {
+        this.userGroup = userGroup;
+    }
+
+    @Builder
+    public Enrollment(Long id, User user, UserGroup userGroup, LocalDateTime enrolledAt, boolean accepted, boolean attended){
+        this.id = id;
+        this.user = user;
+        this.userGroup = userGroup;
+        this.enrolledAt = enrolledAt;
+        this.accepted = accepted;
+        this.attended = attended;
+    }
+}
