0514aa90f4d7bd1a087448d3a0ddc799f43f993a
Jinnie-J <jjesweet@naver.com>
2022-12-14 13:10:23 +0900

    UserGroup Repository 생성

---
+++ src/main/java/com/project/community/domain/user/repository/UserGroupRepository.java
@@ -0,0 +1,7 @@
+package com.project.community.domain.user.repository;
+
+import com.project.community.domain.user.entity.UserGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
+}
