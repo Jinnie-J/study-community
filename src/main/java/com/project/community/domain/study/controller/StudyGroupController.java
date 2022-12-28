4f87afa7aec3ed4642727248c4865210dda312ea
Jinnie-J <jjesweet@naver.com>
2022-12-28 17:14:56 +0900

    스터디그룹, 유저그룹 Controller 분리

--- src/main/java/com/project/community/domain/study/controller/StudyGroupController.java
+++ src/main/java/com/project/community/domain/study/controller/StudyGroupController.java
@@ -1,16 +1,13 @@
 package com.project.community.domain.study.controller;
 
 import com.project.community.common.CurrentUser;
-import com.project.community.domain.enrollment.entity.Enrollment;
 import com.project.community.domain.study.dto.request.StudyGroupRequest;
 import com.project.community.domain.study.dto.response.StudyGroupResponse;
 import com.project.community.domain.study.service.StudyGroupService;
 import com.project.community.domain.user.entity.User;
 import com.project.community.domain.user.entity.UserGroup;
 import com.project.community.domain.user.repository.UserGroupRepository;
-import com.project.community.domain.user.service.UserGroupService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -24,12 +21,11 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@Log4j2
 public class StudyGroupController {
 
     private final StudyGroupService studyGroupService;
     private final UserGroupRepository userGroupRepository;
-    private final UserGroupService userGroupService;
+
     //스터디 그룹 작성 화면 조회
     @GetMapping("/study-group/new")
     public String createStudyGroupForm(@CurrentUser User user, Model model){
@@ -113,39 +109,5 @@ public class StudyGroupController {
 
         return "redirect:/study-group/{studyGroupId}";
     }
-
-    //스터디 그룹 참가 신청
-    @PostMapping("/study-group/{studyGroupId}/enroll")
-    public String newEnrollment(@CurrentUser User user, @PathVariable("studyGroupId")Long studyGroupId){
-
-        UserGroup userGroup = userGroupRepository.findByStudyGroupId(studyGroupId);
-        userGroupService.newEnrollment(userGroup, user);
-        return "redirect:/study-group/{studyGroupId}";
-    }
-
-    //스터디 그룹 참가 신청 취소
-    @PostMapping("/study-group/{studyGroupId}/disenroll")
-    public String cancelEnrollment(@CurrentUser User user, @PathVariable("studyGroupId") Long studyGroupId){
-        UserGroup userGroup = userGroupRepository.findByStudyGroupId(studyGroupId);
-        userGroupService.cancelEnrollment(userGroup, user);
-        return "redirect:/study-group/{studyGroupId}";
-    }
-
-    //참가 신청 수락
-    @GetMapping("/study-group/{studyGroupId}/user-group/{userGroupId}/enrollments/{enrollmentId}/accept")
-    public String acceptEnrollment(@PathVariable("userGroupId") UserGroup userGroup,
-                                   @PathVariable("enrollmentId") Enrollment enrollment){
-        userGroupService.acceptEnrollment(userGroup,enrollment);
-        return "redirect:/study-group/{studyGroupId}/people";
-    }
-
-    //참가 신청 거절
-    @GetMapping("/study-group/{studyGroupId}/user-group/{userGroupId}/enrollments/{enrollmentId}/reject")
-    public String rejectEnrollment(@PathVariable("userGroupId") UserGroup userGroup,
-                                   @PathVariable("enrollmentId") Enrollment enrollment){
-        userGroupService.rejectEnrollment(userGroup,enrollment);
-        return "redirect:/study-group/{studyGroupId}/people";
-    }
-
 
 }
