020d52a1e2880fdffbb6981239ccc2b2cf654486
Jinnie-J <jjesweet@naver.com>
2023-01-04 15:39:28 +0900

    User 정보 수정을 위한 Profile 엔티티 추가

---
+++ src/main/java/com/project/community/domain/user/dto/Profile.java
@@ -0,0 +1,39 @@
+package com.project.community.domain.user.dto;
+
+import com.project.community.domain.location.entity.Location;
+import com.project.community.domain.skill.entity.Skill;
+import com.project.community.domain.user.entity.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+public class Profile {
+
+    @NotBlank
+    @Length(min = 3, max = 20)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
+    private String nickname;
+
+    private Location location;
+
+    private Set<Skill> skills;
+
+    @NotBlank
+    @Length(min = 8, max = 50)
+    private String password;
+
+    public Profile(User user){
+        this.nickname = user.getNickname();
+        this.location = user.getLocation();
+        this.skills = user.getSkills();
+        this.password = user.getPassword();
+    }
+
+
+}
