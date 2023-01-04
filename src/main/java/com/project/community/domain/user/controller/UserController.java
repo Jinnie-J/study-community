954a27816d4e57b729089b33c2ef139e46a13999
Jinnie-J <jjesweet@naver.com>
2023-01-04 15:47:27 +0900

    User 지역, 기술 관계 설정

--- src/main/java/com/project/community/domain/user/controller/UserController.java
+++ src/main/java/com/project/community/domain/user/controller/UserController.java
@@ -1,27 +1,44 @@
 package com.project.community.domain.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.community.common.CurrentUser;
+import com.project.community.domain.location.entity.Location;
+import com.project.community.domain.location.service.LocationService;
+import com.project.community.domain.skill.entity.Skill;
+import com.project.community.domain.skill.service.SkillService;
 import com.project.community.domain.user.SignUpFormValidator;
-import com.project.community.domain.user.dto.request.SignUpForm;
+import com.project.community.domain.user.dto.Profile;
+import com.project.community.domain.user.dto.SignUpForm;
 import com.project.community.domain.user.entity.User;
+import com.project.community.domain.user.repository.UserRepository;
 import com.project.community.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
     private final SignUpFormValidator signUpFormValidator;
+    private final UserRepository userRepository;
+    private final SkillService skillService;
+    private final LocationService locationService;
+    private final ObjectMapper objectMapper;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -55,6 +72,46 @@ public class UserController {
         User user = userService.saveNewUser(signUpForm);
         userService.login(user);
         return "redirect:/";
+    }
+
+    @GetMapping("/user/{nickname}")
+    public String viewProfile(@PathVariable String nickname, Model model, @CurrentUser User user){
+        User userByNickname = userRepository.findByNickname(nickname);
+        if(nickname == null){
+            throw new IllegalArgumentException(nickname + "에 해당하는 사용자가 없습니다.");
+        }
+        model.addAttribute(userByNickname);
+        model.addAttribute("isOwner", userByNickname.equals(user));
+        return "user/profile";
+    }
+
+    @GetMapping("/profile/update")
+    public String profileUpdateForm(Model model, @CurrentUser User user) throws JsonProcessingException {
+        model.addAttribute(user);
+        model.addAttribute(new Profile(user));
+
+        Set<Skill> skills = userService.getSkills(user.getId());
+        model.addAttribute("skills", skills.stream().map(Skill::getTitle).collect(Collectors.toList()));
+
+        List<String> allSkills = skillService.getAllSkills();
+        model.addAttribute("skillList", objectMapper.writeValueAsString(allSkills));
+
+        List<Location> allLocations = locationService.findAll();
+        model.addAttribute("locationList", allLocations);
+        return "user/profile-modify-form";
+    }
+
+    @PostMapping("/profile/update")
+    public String updateProfile(@CurrentUser User user, @Valid @ModelAttribute Profile profile, Errors errors,
+                                Model model, RedirectAttributes attributes) throws ParseException {
+        if (errors.hasErrors()) {
+            model.addAttribute(user);
+            log.info("에러"+errors);
+            return "user/profile-modify-form";
+        }
+        userService.updateProfile(user, profile);
+        attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
+        return "redirect:/profile/update";
     }
 
 
--- src/main/java/com/project/community/domain/user/service/UserService.java
+++ src/main/java/com/project/community/domain/user/service/UserService.java
@@ -1,13 +1,20 @@
 package com.project.community.domain.user.service;
 
-import com.project.community.domain.user.dto.request.SignUpForm;
+import com.project.community.domain.skill.entity.Skill;
+import com.project.community.domain.user.dto.Profile;
+import com.project.community.domain.user.dto.SignUpForm;
 import com.project.community.domain.user.entity.User;
+import org.json.simple.parser.ParseException;
 
 import javax.validation.Valid;
+import java.util.Set;
 
 public interface UserService {
 
     void login(User user);
     User saveNewUser(@Valid SignUpForm signUpForm);
 
+    void updateProfile(User user, Profile profile) throws ParseException;
+
+    Set<Skill> getSkills(Long userId);
 }
