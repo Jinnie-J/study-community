071e8913a0c13d7482ed8524c2a9eadb0353d159
Jinnie-J <jjesweet@naver.com>
2023-01-30 11:18:07 +0900

    알림 인터셉터 postHandle 구현

--- src/main/java/com/project/community/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/project/community/domain/notification/repository/NotificationRepository.java
@@ -1,7 +1,9 @@
 package com.project.community.domain.notification.repository;
 
 import com.project.community.domain.notification.Notification;
+import com.project.community.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByUserAndChecked(User user, boolean checked);
 }
