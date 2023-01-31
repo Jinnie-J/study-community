e3ecdd188248ba78fbf15632b985621fd5b3d5c1
Jinnie-J <jjesweet@naver.com>
2023-01-31 10:55:51 +0900

    알림 삭제 로직 구현

--- src/main/java/com/project/community/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/project/community/domain/notification/repository/NotificationRepository.java
@@ -3,7 +3,16 @@ package com.project.community.domain.notification.repository;
 import com.project.community.domain.notification.Notification;
 import com.project.community.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     long countByUserAndChecked(User user, boolean checked);
+    @Transactional
+    List<Notification> findByUserAndCheckedOrderByCreatedDateTimeDesc(User user, boolean checked);
+
+    @Transactional
+    void deleteByUserAndChecked(User user, boolean checked);
 }
